ff9432bd5f18793e380a989bf21bcca446d3cd41
Teravia <teravia.vip@gmail.com>
2026-08-09 20:42:38 +0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  // TODO: feed (chronological / community popular / latest / unanswered / followed)
+  return (
+    <main>
+      <h1>SAWALA</h1>
+      <p>Community built on contribution.</p>
+    </main>
+  );
+}
