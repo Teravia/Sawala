5e2c5cd121aa96f7e3316966ef5a59b4f060f3a9
Teravia <teravia.vip@gmail.com>
2026-08-09 21:18:38 +0700

    Delete src/modules/moderation/communities.test.ts

--- src/modules/moderation/communities.test.ts
+++
@@ -1,6 +0,0 @@
-// Unit tests for "communities" business rules.
-// TODO: cover acceptance criteria from /docs/REFACTOR.md
-
-describe("communities module", () => {
-  it.todo("implement business rule tests");
-});
